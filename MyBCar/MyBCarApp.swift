3c978254f2dcb7b702d3d66afa682a3218841d08
Vojin <dvojinovic38@gmail.com>
2024-12-25 16:17:19 +0100

    Initial XCode project push

---
+++ MyBCar/MyBCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBCarApp.swift
+//  MyBCar
+//
+//  Created by Dusan Vojinovic on 25.12.24..
+//
+
+import SwiftUI
+
+@main
+struct MyBCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
